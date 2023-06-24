b3660a522bebc365559bdbe31588b9cf634da5a7
Seraphim9 <82789712+Seraphim9@users.noreply.github.com>
2023-06-24 14:25:22 +0700

    update

--- lib/constants.dart
+++ lib/constants.dart
@@ -26,14 +26,17 @@ const defaultDuration = Duration(milliseconds: 250);
 // Form Error
 final RegExp emailValidatorRegExp =
     RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
-const String kEmailNullError = "Please Enter your email";
-const String kInvalidEmailError = "Please Enter Valid Email";
-const String kPassNullError = "Please Enter your password";
-const String kShortPassError = "Password is too short";
-const String kMatchPassError = "Passwords don't match";
-const String kNamelNullError = "Please Enter your name";
-const String kPhoneNumberNullError = "Please Enter your phone number";
-const String kAddressNullError = "Please Enter your address";
+const String kEmailNullError = "Masukkan email anda";
+const String kInvalidEmailError = "Masukkan email anda sekali lagi";
+const String kPassNullError = "Masukkan password";
+const String kShortPassError = "Password terlalu pendek";
+const String kMatchPassError = "Password tidak sesuai";
+const String kNamelNullError = "Masukkan nama anda";
+const String kPhoneNumberNullError = "Masukkan nomor telepon anda";
+const String kPhoneNumberInvalid = "Nomor telepon tidak valid !";
+const String kAddressNullError = "Masukkan alamat anda";
+const String kUsernameInput = "Masukkan username anda";
+const String kUsernameUsed = "Username tidak tersedia !";
 
 final otpInputDecoration = InputDecoration(
   contentPadding:
